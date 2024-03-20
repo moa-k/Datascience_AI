d7c63112cc784d1525b017d77cc81110d67a4b21
moa-k <moabackstrom@hotmail.com>
2024-03-20 17:59:13 +0100

    os update

--- Assignment1/denpendency_ratio.py
+++ Assignment1/denpendency_ratio.py
@@ -1,9 +1,13 @@
 import numpy as np
 import pandas as pd
 import matplotlib as plt
+import os
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+file_path = os.path.join(current_directory, 'swedish_population_by_year_and_sex_1860-2022.csv')  
 
 # Read CSV file
-swedish_population = pd.read_csv("swedish_population_by_year_and_sex_1860-2022.csv")
+swedish_population = pd.read_csv(file_path)
 
 print(swedish_population)
 
