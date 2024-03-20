239b654e7be1d34e9d383084c45c75b04737b448
albaenglen <albaenglen99@hotmail.com>
2024-03-20 17:55:05 +0100

    kan printa data

--- Assignment1/denpendency_ratio.py
+++ Assignment1/denpendency_ratio.py
@@ -1,15 +1,14 @@
 import numpy as np
 import pandas as pd
 import matplotlib as plt
+import matplotlib.pyplot as plt
 
 # Read CSV file
-swedish_population = pd.read_csv("swedish_population_by_year_and_sex_1860-2022.csv")
-
+swedish_population = pd.read_csv("/Users/albaenglen/Documents/GitHub/Datascience_AI/Assignment1/swedish_population_by_year_and_sex_1860-2022.csv")
 print(swedish_population)
 
-'''plt.plot(swedish_population['x'], swedish_population['y'])
+plt.plot(swedish_population['x'], swedish_population['y'])
 plt.xlabel('Year')
 plt.ylabel('Ratio')
 plt.title('Dependency ratio of Sweden from 1860 to 2022')
-plt.show()'''
-
+plt.show()
