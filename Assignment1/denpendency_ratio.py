3915f829c3bcf9c53b4edcc36dd494fbadccb5f3
albaenglen <albaenglen99@hotmail.com>
2024-03-20 17:36:41 +0100

    lagt in fil

--- Assignment1/denpendency_ratio.py
+++ Assignment1/denpendency_ratio.py
@@ -2,8 +2,14 @@ import numpy as np
 import pandas as pd
 import matplotlib as plt
 
-swedish_population = pd.read_csv("swedish_population_by_year_and_sex_1860-2022")
+# Read CSV file
+swedish_population = pd.read_csv("swedish_population_by_year_and_sex_1860-2022.csv")
 
-#yeehaaaw
+print(swedish_population)
 
+'''plt.plot(swedish_population['x'], swedish_population['y'])
+plt.xlabel('Year')
+plt.ylabel('Ratio')
+plt.title('Dependency ratio of Sweden from 1860 to 2022')
+plt.show()'''
 
