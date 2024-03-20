d42c7b49880d51468391afde686d797ecf74caab
moa-k <moabackstrom@hotmail.com>
2024-03-20 22:23:03 +0100

    mjukstart

--- Assignment1/denpendency_ratio.py
+++ Assignment1/denpendency_ratio.py
@@ -7,12 +7,15 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_directory, 'swedish_population_by_year_and_sex_1860-2022.csv')  
 
 # Read CSV file
-swedish_population = pd.read_csv(file_path)
+swe_pop = pd.read_csv(file_path) #swedish population
 
-print(swedish_population)
+print('Dimensions:',swe_pop.ndim, 'Shape:', swe_pop.shape, 'Size:', swe_pop.size, 'Type:', swe_pop.dtypes)
 
-plt.plot(swedish_population['x'], swedish_population['y'])
+
+
+'''plt.plot(swedish_population['x'], swedish_population['y'])
 plt.xlabel('Year')
 plt.ylabel('Ratio')
 plt.title('Dependency ratio of Sweden from 1860 to 2022')
 plt.show()
+'''
